de6718d48fd49186de6e9418926d8e28652a2506
Aditya Narwariya <adityanarwariya500@gmail.com>
2026-07-10 13:45:19 +0530

    Time: 30 ms (28.25%), Space: 93.8 MB (5.03%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -26,46 +26,12 @@ public:
         }
         return ans;
     }
-    vector<int> getNLR(vector<int> arr){
-        stack<int>st;
-        vector<int>ans(arr.size());
-        for(int i=arr.size()-1;i>=0;i--){
-            while(!st.empty() && st.top()<=arr[i]){
-                st.pop();
-            }
-            if(st.empty()){
-                ans[i]=arr[i];
-            }
-            else{
-                ans[i]=st.top();
-            }
-            st.push(arr[i]);
-        }
-        return ans;
-    }
-    vector<int> getNLL(vector<int> arr){
-        stack<int>st;
-        vector<int>ans(arr.size());
-        for(int i=0;i<arr.size();i++){
-            while(!st.empty() && st.top()<arr[i]){
-                st.pop();
-            }
-            if(st.empty()){
-                ans[i]=arr[i];
-            }
-            else{
-                ans[i]=st.top();
-            }
-            st.push(arr[i]);
-        }
-        return ans;
-    }
+  
 
     int largestRectangleArea(vector<int>& h) {
         vector<int>NSR = getNSR(h);
         vector<int>NSL = getNSL(h);
-        vector<int>NLR = getNLR(h);
-        vector<int>NLL = getNLL(h);
+        
         int ans = 0;
         for(int i=0;i<h.size();i++){
             int width = NSR[i]-NSL[i]-1;
