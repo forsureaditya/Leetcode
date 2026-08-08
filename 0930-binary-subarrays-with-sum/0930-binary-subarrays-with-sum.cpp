20a8bcef326eb6ec2beed86d09cffa730604b87a
Aditya Narwariya <adityanarwariya500@gmail.com>
2026-08-08 12:05:27 +0530

    Time: 0 ms (100%), Space: 34.8 MB (35.6%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-      unordered_map<int,int>mpp;
-      int i=0;
-      int sum = 0;
-      mpp[0]++;
-      int cnt = 0;
-      while(i<nums.size()){
-        sum+=nums[i];
-        int diff = sum - goal;
-        if(mpp.find(diff)!=mpp.end()){
-            cnt+=mpp[diff];
+    int func(vector<int>nums,int goal){
+        if(goal<0) return 0;
+        int l = 0;
+        int r = 0;
+        int cnt = 0;
+        int sum = 0;
+        while(r<nums.size()){
+            sum+=nums[r];
+            while(sum>goal){
+                sum-=nums[l];
+                l++;
+            }
+            cnt+=r-l+1;
+            r++;
         }
-        
-        mpp[sum]++;
-        i++;
-      }
-      return cnt;
+        return cnt;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return func(nums,goal) - func(nums,goal-1);
     }
 };
