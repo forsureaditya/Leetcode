a9f2248859c4588dc7f6db27dd2520f7c1b588cc
Aditya Narwariya <adityanarwariya500@gmail.com>
2026-08-30 00:12:37 +0530

    Time: 361 ms (95.51%), Space: 478.9 MB (60.15%) - LeetHub

---
+++ 4035-maximum-valid-split-positions-i/4035-maximum-valid-split-positions-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int func(vector<int>nums , int i){
+        if(i!=-1){
+            nums.erase(nums.begin()+i);
+        }
+        int cnt = 0;
+        vector<int>prefix(nums.size());
+        vector<int>suffix(nums.size());
+        prefix[0] = nums[0];
+        suffix[nums.size()-1] = nums[nums.size()-1];
+        for(int i=1;i<nums.size();i++){
+            prefix[i] = __gcd(nums[i],prefix[i-1]);
+        }
+        for(int i=nums.size()-2;i>=0;i--){
+            suffix[i] = __gcd(nums[i],suffix[i+1]);
+        }
+        for(int i=0;i<nums.size()-1;i++){
+            if(prefix[i]==suffix[i+1]){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+    int maxValidSplits(vector<int>& nums) {
+        int ans = func(nums,-1);
+        for(int i=0;i<nums.size();i++){
+            ans = max(ans,func(nums,i));
+        }
+        return ans;
+    }
+};
