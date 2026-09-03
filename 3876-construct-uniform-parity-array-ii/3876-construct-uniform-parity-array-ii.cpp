f9a6f6d33ce5188480aca614d3eda17f92307482
Aditya Narwariya <adityanarwariya500@gmail.com>
2026-09-03 17:15:30 +0530

    Time: 7 ms (51.05%), Space: 165.9 MB (31.94%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -2,13 +2,9 @@ class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
         int cntodd = 0;
-        for(auto it: nums1){
-            if(it%2!=0) cntodd++;
-        }
+        for(auto it: nums1) if(it%2!=0) cntodd++;
         int mini = *min_element(nums1.begin(),nums1.end());
-        if(mini%2 == 1){
-            return true;
-        }
+        if(mini%2 == 1)return true;
         if(mini%2==0 && cntodd == 0) return true;
         return false;
     }
