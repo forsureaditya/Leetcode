32975f1ecd8fb016eed3f3de666563068a9101e9
Aditya Narwariya <adityanarwariya500@gmail.com>
2026-09-13 09:45:14 +0530

    Time: 63 ms (5.51%), Space: 73.7 MB (17.44%) - LeetHub

---
+++ 4052-cyclically-shift-rows-and-columns/4052-cyclically-shift-rows-and-columns.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> cyclicShift(int n, vector<vector<int>>& grid, vector<int>& rowShift, vector<int>& colShift) {
+        vector<vector<int>>ans(n,vector<int>(n));
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[i].size();j++){
+               ans[i][(j-rowShift[i]+n)%n] = grid[i][j]; 
+            }
+        }
+        // try to cout.
+        vector<vector<int>>fans(n,vector<int>(n));
+        for(int i=0;i<grid.size();i++){//act like row
+            for(int j=0;j<grid.size();j++){//act like col
+                fans[(j-colShift[i]+n)%n][i] = ans[j][i];
+            }
+            
+        }
+        for(int i=0;i<grid.size();i++){//act like row
+            for(int j=0;j<grid.size();j++){//act like col
+                cout<<fans[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        return fans;
+    }
+};
