44490510a9f842e6f7f2daad933a87c95fd703fe
Aditya Narwariya <adityanarwariya500@gmail.com>
2026-09-19 08:54:59 +0530

    Time: 0 ms (100%), Space: 15.6 MB (46.66%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     bool winnerOfGame(string colors) {
         int cnta = 0;
-        string stra = "AAA";
         int cntb = 0;
-        string strb = "BBB";
-        for(int i=0;i<=(int)colors.size()-3;i++){
-            if(colors.substr(i,3)==stra){
+        for(int i=1;i<=colors.size()-1;i++){
+            if(colors[i]==colors[i-1] && colors[i+1]==colors[i] && colors[i]=='A'){
                 cnta++;
             }
-            if(colors.substr(i,3)==strb){
+            if(colors[i]==colors[i-1] && colors[i+1]==colors[i] && colors[i]=='B'){
                 cntb++;
             }
         }
